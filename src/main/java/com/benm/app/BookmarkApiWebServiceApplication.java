a924ce4d5a63a67b160cbe89a90034c0b4781996
Ben Marcotte <33464129+bmarcotte@users.noreply.github.com>
2018-03-19 13:30:52 -0700

    Results of Sprint Initializr run
    Checking in the results of running the Sprint Initializr
    (http://start.spring.io/) project generator.  Options selected during
    generation included: Maven, Sprint Boot 2.0.0, Java 8, Jar packaging,
    and the "Web" and "JPA" dependencies.

---
+++ src/main/java/com/benm/app/BookmarkApiWebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.benm.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkApiWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkApiWebServiceApplication.class, args);
+	}
+}
